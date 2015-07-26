a58127b66a527aacd5dec7703fa6634b92555a25
schoenegger <thomas.schoenegger@gmail.com>
2015-07-26 22:13:59 +0200

    merge

--- Battleship/src/view/settings/StartSettingsWindow.java
+++ Battleship/src/view/settings/StartSettingsWindow.java
@@ -206,8 +206,10 @@ public class StartSettingsWindow extends JDialog
 
 		refGameLogic.getgameSoundPlayer().addActionListener(
 				this.viewSettingsListener);
-		frmSettings.addWindowListener(this.viewSettingsListener);
+		frmSettings.addWindowListener(viewSettingsListener);
+
 		btnSave.setMnemonic(KeyEvent.VK_S);
+
 		btnSave.addKeyListener(this.viewSettingsListener);
 		btnSave.addActionListener(this.viewSettingsListener);
 		btnSave.setActionCommand(Definitions.SAVE_BUTTON_START_SETTINGS_VIEW);
