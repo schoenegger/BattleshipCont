2880cd05dcd62fc0d17a96c22e1dce760aed36c4
styriandidi <dietmar.neubauer@edu.fh-joanneum.at>
2015-07-26 09:53:17 +0200

    Gamesoundplayer refactor

--- Battleship/src/gameSounds/GameSoundPlayer.java
+++ Battleship/src/gameSounds/GameSoundPlayer.java
@@ -4,28 +4,89 @@ import java.io.File;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JButton;
 
-public class GameSoundPlayer
+import logging.Logging;
+
+public class GameSoundPlayer extends JButton
 {
 	// Didi ADD
 	public final static String SOUND_MENUE_WAV = "sound_menue.wav";
 	public final static String SOUND_SETTING_WAV = "sound_settings.wav";
-
+	public final static String SOUND_GAME_WAV = "sound_game.wav";
+	public final static String SOUND_CANNON_HIT = "sound_cannon_hit";
+	public final static String SOUND_CANNON_MISS = "sound_cannon_miss";
 	// public final static String SOUND_GAME_WAV = "";
 
-	private enum SoundTypes
-	{
-		BOMB, BACKGROUND, WATERSHOOT;
-	}
-
 	private Clip soundClip;
+	private boolean soundIsOn;
+	private String currentWavFileNameToPlay;
+
+	// private enum SoundTypes
+	// {
+	// BOMB, BACKGROUND, WATERSHOOT;
+	// }
 
 	public GameSoundPlayer()
 	{
+		soundIsOn = true;
+		currentWavFileNameToPlay = GameSoundPlayer.SOUND_MENUE_WAV;
 		startBackgroundSound(GameSoundPlayer.SOUND_MENUE_WAV);
 	}
 
+	public void turnSoundOnOrOFF()
+	{
+		if (soundIsOn)
+		{
+			soundIsOn = false;
+			stopBackGroundSounds();
+		}
+		else
+		{
+			soundIsOn = true;
+			this.startBackgroundSound(this.currentWavFileNameToPlay);
+		}
+	}
+
+	/**
+	 * Use this Method in your code only by switching windows
+	 * (open-close-events).
+	 * 
+	 * @see #turnSoundOnOrOFF()
+	 */
+	public void stopBackGroundSounds()
+	{
+		soundClip.stop();
+	}
+
 	public void startBackgroundSound(String wavFilename)
+	{
+		currentWavFileNameToPlay = wavFilename;
+		if (soundIsOn)
+		{
+			synchronized (wavFilename)
+			{
+				try
+				{
+					this.soundClip = AudioSystem.getClip();
+					this.soundClip.open(AudioSystem
+							.getAudioInputStream(new File("sound\\"
+									+ wavFilename)));
+				}
+				catch (Exception e)
+				{
+					Logging.writeErrorMessage("Methode startBackgroundSound Cannot play sound "
+							+ wavFilename + " !");
+					System.err.println("Error: " + e.getMessage());
+					e.printStackTrace();
+				}
+				soundClip.loop(Clip.LOOP_CONTINUOUSLY);
+			}
+		}
+
+	}
+
+	public void startBombSound(String wavFilename)
 	{
 		synchronized (wavFilename)
 		{
@@ -37,24 +98,12 @@ public class GameSoundPlayer
 			}
 			catch (Exception e)
 			{
-				// logger no file loading possible
+				Logging.writeErrorMessage("Methode startBombSound Cannot play sound "
+						+ wavFilename + " !");
+				System.err.println("Error: " + e.getMessage());
 				e.printStackTrace();
 			}
-			soundClip.loop(Clip.LOOP_CONTINUOUSLY);
 		}
 
 	}
-
-	public void startBombSound()
-	{
-
-	}
-
-	public void stopBackGroundSounds()
-	{
-		soundClip.stop();
-	}
-	// Clip clip = AudioSystem.getClip();
-	// clip.open(AudioSystem.getAudioInputStream(soundFileName));
-
 }
