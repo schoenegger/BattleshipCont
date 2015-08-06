458959ee570f4db7b305d41e53563caecf5e64fd
schoenegger <thomas.schoenegger@gmail.com>
2015-08-06 22:44:41 +0200

    merge application

--- Battleship/src/view/GlobalStrings/Definitions.java
+++ Battleship/src/view/GlobalStrings/Definitions.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package view.GlobalStrings;
 
 public class Definitions
@@ -24,29 +23,30 @@ public class Definitions
 	public final static int MAX_YELLOW_SUBMARINE = 3;
 	public final static int MAX_COUNT_SHIPS_ON_FIELD = 6;
 }
-=======
-package view.GlobalStrings;
 
-public class Definitions
-{
-	public final static String CONN_HOST = "ConnHost";
-	public final static String CONN_Client = "ConnClient";
-	public final static String CONN_CPU = "ConnCpu";
-
-	public final static String PLAYER_VS_PLAYER = "PlayerVsPlayer";
-	public final static String PLAYER_VS_COM = "PlayerVsCom";
-	public final static String SETTING_START_VIEW = "SettingsStartView";
-
-	public final static String SAVE_BUTTON_START_SETTINGS_VIEW = "SaveButtonStartSettingsView";
-	public final static String BUTTON_SET_SHIP = "ButtonSetShip";
-	public final static String BUTTON_ATTAC = "ButtonAttac";
-
-	public final static String TURN_MUSIC_ON_OFF = "TurnMusicOnOff";
-
-	// Count of ships in Game
-	public final static int MAX_AIRCARRIER = 1;
-	public final static int MAX_DESTROYER = 2;
-	public final static int MAX_YELLOW_SUBMARINE = 3;
-	public final static int MAX_COUNT_SHIPS_ON_FIELD = 6;
-}
->>>>>>> 25df602e181fffb55d3dda16b082f021664f2d05
+// package view.GlobalStrings;
+//
+// public class Definitions
+// {
+// public final static String CONN_HOST = "ConnHost";
+// public final static String CONN_Client = "ConnClient";
+// public final static String CONN_CPU = "ConnCpu";
+//
+// public final static String PLAYER_VS_PLAYER = "PlayerVsPlayer";
+// public final static String PLAYER_VS_COM = "PlayerVsCom";
+// public final static String SETTING_START_VIEW = "SettingsStartView";
+//
+// public final static String SAVE_BUTTON_START_SETTINGS_VIEW =
+// "SaveButtonStartSettingsView";
+// public final static String BUTTON_SET_SHIP = "ButtonSetShip";
+// public final static String BUTTON_ATTAC = "ButtonAttac";
+//
+// public final static String TURN_MUSIC_ON_OFF = "TurnMusicOnOff";
+//
+// // Count of ships in Game
+// public final static int MAX_AIRCARRIER = 1;
+// public final static int MAX_DESTROYER = 2;
+// public final static int MAX_YELLOW_SUBMARINE = 3;
+// public final static int MAX_COUNT_SHIPS_ON_FIELD = 6;
+// }
+// >>>>>>> 25df602e181fffb55d3dda16b082f021664f2d05
