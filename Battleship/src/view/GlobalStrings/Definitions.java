43a30aebba13a0fb984179a086e0f87c52dcba05
schoenegger <thomas.schoenegger@gmail.com>
2015-08-06 21:55:34 +0200

    merge

--- Battleship/src/view/GlobalStrings/Definitions.java
+++ Battleship/src/view/GlobalStrings/Definitions.java
@@ -11,9 +11,14 @@ public class Definitions
 	public final static String SETTING_START_VIEW = "SettingsStartView";
 
 	public final static String SAVE_BUTTON_START_SETTINGS_VIEW = "SaveButtonStartSettingsView";
-	public final static String SHOW_LOG_BUTTON_START_SETTINGS_VIEW = "ShowLogButtonStartSettingsView";
 	public final static String BUTTON_SET_SHIP = "ButtonSetShip";
 	public final static String BUTTON_ATTAC = "ButtonAttac";
 
 	public final static String TURN_MUSIC_ON_OFF = "TurnMusicOnOff";
+
+	// Count of ships in Game
+	public final static int MAX_AIRCARRIER = 1;
+	public final static int MAX_DESTROYER = 2;
+	public final static int MAX_YELLOW_SUBMARINE = 3;
+	public final static int MAX_COUNT_SHIPS_ON_FIELD = 6;
 }
